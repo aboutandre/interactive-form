0942beffbd76e9e0777113dabc49248f85ea3055
Andre Oliveira <github@aboutandre.com>
2018-10-28 11:45:52 +0100

    ”Job Role” section
    Include a text field that will be revealed when the "Other" option is selected from the "Job Role" drop down menu.
    Give the field an id of “other-title,” and add the placeholder text of "Your Job Role".

--- js/script.js
+++ js/script.js
@@ -3,4 +3,37 @@ const initialFocus = function () {
     console.log('Set focus!');
 };
 
+const jobTitleSelection = document.getElementById('title');
+
+// Show an element
+const showElement = function (el) {
+    const element = document.getElementById(el);
+    element.classList.add('is-visible');
+    element.classList.remove('is-hidden');
+};
+
+// Hide an element
+const hideElement = function (el) {
+    const element = document.getElementById(el);
+    element.classList.add('is-hidden');
+    element.classList.remove('is-visible');
+};
+
+// Toggle element visibility
+const toggleElement = function (el) {
+    const element = document.getElementById(el);
+    element.classList.toggle('is-visible');
+    element.classList.toggle('is-hidden');
+};
+
+jobTitleSelection.addEventListener("change", function () {
+    if (this.value === 'other') {
+        showElement('other-title');
+    } else {
+        hideElement('other-title');
+    }
+});
+
+// We initialize all our functions
 window.onload = initialFocus();
+hideElement('other-title');
