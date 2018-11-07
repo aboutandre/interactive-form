385bb26a1d326b90cf5771dec69590e20fd0e79c
Andre Oliveira <github@aboutandre.com>
2018-11-07 22:24:05 +0100

    Add comments and reorganise the app JavaScript

--- js/script.js
+++ js/script.js
@@ -1,9 +1,16 @@
+/******************************************
+Treehouse Techdegree:
+FSJS project 3 - Build an Interactive Form
+******************************************/
+
+// Here we create all the variables that we need for our app
 const nameField = document.getElementById('name');
 const emailField = document.getElementById('mail');
 const jobTitleSelection = document.getElementById('title');
 const otherJobTitle = document.getElementById('other-title');
 const tShirtColors = document.getElementById('colors-js-puns');
 const tShirtDesignSelection = document.getElementById('design');
+// We make a copy of the t-shirt colors to be able to remove items depending on the design selection
 const tShirtColorsTemplate = tShirtColors.innerHTML;
 const paymentMethod = document.getElementById('payment');
 const paymentCC = document.getElementById('credit-card');
@@ -21,41 +28,36 @@ const activityExpress = document.getElementsByName('express')[0];
 const activityNode = document.getElementsByName('node')[0];
 const activityBuildTools = document.getElementsByName('build-tools')[0];
 const activityNPM = document.getElementsByName('npm')[0];
-
 const activitiesListener = activities.getElementsByTagName('INPUT');
-const createSumContainer = document.createElement('div');
-createSumContainer.id = 'sum-container';
 
+// We require a couple of elements that are not present in the DOM at load time
 const activitiesErrorContainer = document.createElement('div');
 activitiesErrorContainer.id = 'activities-error';
 activities.appendChild(activitiesErrorContainer);
 
-const submitButton = document.querySelectorAll('[type=submit]')[0];
+const createSumContainer = document.createElement('div');
+createSumContainer.id = 'sum-container';
 
-console.log(activitiesListener);
+const submitButton = document.querySelectorAll('[type=submit]')[0];
 
 // Set focus on the name input field
 const initialFocus = function () {
     nameField.focus();
 };
 
-// Show an element
+// Function used to show an element
 const showElement = function (elementID) {
     elementID.classList.add('is-visible');
     elementID.classList.remove('is-hidden');
 };
 
-// Hide an element
+// Function used to hide an element
 const hideElement = function (elementID) {
     elementID.classList.add('is-hidden');
     elementID.classList.remove('is-visible');
 };
 
-const initializeTshirtColors = function () {
-    tShirtColors.innerHTML = '';
-    tShirtColors.innerHTML = tShirtColorsTemplate;
-};
-
+// This function checks if two tracks given have conflicting time slots
 const parallelTracks = function (firstTrack, secondTrack) {
     firstTrack.onchange = function () {
         this.checked ? (secondTrack.disabled = true) : (secondTrack.disabled = false);
@@ -65,6 +67,7 @@ const parallelTracks = function (firstTrack, secondTrack) {
     }
 }
 
+// If the user selects a job title "other", then we show the "Other Job" field
 jobTitleSelection.addEventListener('change', function () {
     if (this.value === 'other') {
         showElement(otherJobTitle);
@@ -73,139 +76,111 @@ jobTitleSelection.addEventListener('change', function () {
     }
 });
 
+// When this function is called we set the content of the t-shirt design `select` dropdown to the template copy that we did with `tShirtColorsTemplate`
+const initializeTshirtColors = function () {
+    tShirtColors.innerHTML = '';
+    tShirtColors.innerHTML = tShirtColorsTemplate;
+};
+
+// We listen for any changes on the t-shirt design `select` dropdown
 tShirtDesignSelection.addEventListener('change', function () {
     showElement(tShirtColors);
     initializeTshirtColors();
+    // This will controll the second dropdown to show only `js puns` tshirts
     if (this.value === 'js puns') {
         const jsPunsColors = document.getElementById('color');
         for (let i = 0; i < jsPunsColors.length; i++) {
             let currentColor = jsPunsColors.options[i].value;
+            // We remove all the colors from the oposite group
             if (currentColor == 'tomato' || currentColor == 'steelblue' || currentColor == 'dimgrey') {
                 jsPunsColors.options[i].remove();
                 i--; // we decrease i, otherwise we would jump one of the colors
             }
         }
+        // This will controll the second dropdown to show only `heart js` tshirts
     } else if (this.value === 'heart js') {
         const jsPunsColors = document.getElementById('color');
         for (let i = 0; i < jsPunsColors.length; i++) {
             let currentColor = jsPunsColors.options[i].value;
+            // We remove all the colors from the oposite group
             if (currentColor == 'cornflowerblue' || currentColor == 'darkslategrey' || currentColor == 'gold') {
                 jsPunsColors.options[i].remove();
                 i--; // we decrease i, otherwise we would jump one of the colors
             }
         }
     } else {
+        // If the users doesnt select either designs we hide the dropdown
         hideElement(tShirtColors);
     }
 });
 
+// We create a sum element to be used for the price of all chose activities
 const appendSum = function (totalPrice) {
+    // If there was a sum already, we get rid of it
     createSumContainer.innerHTML = '';
     const sum = `<h3 class="total-sum">Total: $${totalPrice}</h3>`
     createSumContainer.innerHTML = sum;
 };
 
+// We listen for changes in the activities selection
 activities.addEventListener('change', function () {
+    // We set the initial cost to zero
     let price = 0;
+    // Since the "Main Conference" is the only one that costs $200 it gets it's own conditional
     if (activityConference.checked) {
         price += 200;
     }
+    // For all the other activities we sum the price in $100 increments
     for (let i = 1; activitiesListener.length > i; i++) {
         if (activitiesListener[i].checked) {
             price += 100;
         }
     }
+    // Now we show the price
     appendSum(price);
+    // If the user deselects all activities she will get an error
     validateActivities();
 });
 
+// We set the default payment to credit card
 const defaultPayment = function () {
+    // We always hide all the methods that we don't need
     hidePaymentMethods();
+    // We set the payment method dropdown to credit card
     paymentMethod.value = 'credit card';
+    // And we show the credit card relevant fields
     showElement(paymentCC);
 }
 
+// In this function we hide all the payment methods
 const hidePaymentMethods = function () {
     hideElement(paymentCC);
     hideElement(paymentPayPal);
     hideElement(paymentBitcoin);
 }
 
+// We listen for changes in the payment dropdown 
 paymentMethod.addEventListener('change', function () {
+    // If CC we re-run the default payment method function
     if (this.value === 'credit card') {
         defaultPayment();
+        // If Paypal we hide all the methods and show only PP
     } else if (this.value === 'paypal') {
         hidePaymentMethods();
         showElement(paymentPayPal);
+        // The last option is Bitcoin, same as PP we hide all and show only Bitcoin
     } else {
         hidePaymentMethods();
         showElement(paymentBitcoin);
     }
 });
 
-const errorMessage = function (errorField, errorText) {
-    const textContent = document.createElement('p');
-    textContent.innerHTML = errorText;
-    textContent.classList.add('error-message');
-    textContent.id = errorField.id + '-error';
-    errorField.parentNode.insertBefore(textContent, errorField.nextSibling);
-};
-
-const removeErrorMessage = function (errorField) {
-    const errorMessageId = errorField.id + '-error';
-    const errorMessage = document.getElementById(errorMessageId);
-    errorField.parentNode.removeChild(errorMessage);
-}
-
-const validateName = function () {
-    if (nameField.value === '' || nameField.value === null) {
-        validate(false, nameField, 'empty-field', 'Please add a name.');
-    } else {
-        validate(true, nameField);
-    }
-};
-
-const validateEmail = function () {
-    isValid = emailField.checkValidity();
-    if (!isValid || emailField.value === '') {
-        validate(false, emailField, 'invalid-format', 'Please add a valid email.');
-    } else {
-        validate(true, emailField);
-    }
-};
-
-const validateActivities = function () {
-    let anyChecked = false;
-    for (let i = 0; activitiesListener.length > i; i++) {
-        if (activitiesListener[i].checked === true) {
-            anyChecked = true;
-        }
-    }
-    if (anyChecked === false) {
-        validate(false, activitiesErrorContainer, 'missing-activity', 'Please select at least one activity');
-    } else {
-        validate(true, activitiesErrorContainer);
-    }
-};
-
-const validatePayment = function () {
-    if (paymentMethod.value === 'credit card') {
-        validateCCNumber();
-        validateCCZip();
-        validateCvv();
-    }
-};
-
-const validateCCNumber = function () {
-    if (ccNumber.value.length <= 0) {
-        validate(false, ccNumber, 'empty-field', 'Please enter a credit card number.');
-    } else if (13 >= ccNumber.value.length || ccNumber.value.length >= 16) {
-        validate(false, ccNumber, 'wrong-length', 'Please add a card with 13 to 16 digits.');
-    } else {
-        validate(true, ccNumber);
-    }
-};
-
+// This is the validate function
+// It takes four parameters, two mandatory and two optionals
+// 'passed' defines if the condition, well... passes. It's a boolean
+// 'evaluatedElement' is the element that will be evaluated. It uses the variables defined at the top of this file
+// 'errorName' is optional and serves to be help debugging
+// 'errorMessage' is also optional (in case we are removing the error we don't need to pass a message)
 const validate = function (passed, evaluatedElement, errorName = 0, errorMessage = 0) {
     // We need to remove all previous errors from this element
     const evaluatedElementId = evaluatedElement.id;
@@ -229,6 +204,73 @@ const validate = function (passed, evaluatedElement, errorName = 0, errorMessage
     }
 };
 
+// Function to validate if the name is not empty
+const validateName = function () {
+    if (nameField.value === '' || nameField.value === null) {
+        validate(false, nameField, 'empty-field', 'Please add a name.');
+    } else {
+        validate(true, nameField);
+    }
+};
+
+// Function to validate if the email has a valid format
+const validateEmail = function () {
+    // The checkValidity method works using the type attribute of the email field 
+    isValid = emailField.checkValidity();
+    // If it's invalid
+    if (!isValid) {
+        validate(false, emailField, 'invalid-format', 'Please add a valid email.');
+        // If it's empty
+    } else if (nameField.value === '' || nameField.value === null) {
+        validate(false, emailField, 'empty-field', 'Please add an email.');
+    } else {
+        validate(true, emailField);
+    }
+};
+// Function to validate if any activity has been selected 
+const validateActivities = function () {
+    // We create a variable that will change any *any* of the activities has been selected
+    let anyChecked = false;
+    // We loop through all the activities and if any of them are checked, we set the 'anyChecked' to true
+    for (let i = 0; activitiesListener.length > i; i++) {
+        if (activitiesListener[i].checked === true) {
+            anyChecked = true;
+        }
+    }
+    // If none are checked we show an error
+    if (anyChecked === false) {
+        validate(false, activitiesErrorContainer, 'missing-activity', 'Please select at least one activity');
+        // Else we don't show any error or remove the present one 
+    } else {
+        validate(true, activitiesErrorContainer);
+    }
+};
+
+// Function to validate all the credit card fields
+const validatePayment = function () {
+    // We only run if the payment method selected is the credit card
+    if (paymentMethod.value === 'credit card') {
+        validateCCNumber();
+        validateCCZip();
+        validateCvv();
+    }
+};
+
+// Function to validate if the CC number is correct
+const validateCCNumber = function () {
+    // If empty we show an error if the proper message
+    if (ccNumber.value.length <= 0) {
+        validate(false, ccNumber, 'empty-field', 'Please enter a credit card number.');
+        // If the length of the number is not between 13 and 16 we show the proper message
+    } else if (13 >= ccNumber.value.length || ccNumber.value.length >= 16) {
+        validate(false, ccNumber, 'wrong-length', 'Please add a card with 13 to 16 digits.');
+        // Else we don't show any error or remove the present one 
+    } else {
+        validate(true, ccNumber);
+    }
+};
+
+// Function to validate if the CC Postal Code/ZIP is exactly 5 digits long
 const validateCCZip = function () {
     if (ccZip.value.length !== 5) {
         validate(false, ccZip, 'wrong-postal-code', 'Please add a 5 digit ZIP code.');
@@ -237,6 +279,7 @@ const validateCCZip = function () {
     };
 };
 
+// Function to validate if the CC CVV is exactly 3 digits long
 const validateCvv = function () {
     if (ccCvv.value.length !== 3) {
         validate(false, ccCvv, 'wrong-cvv', 'Please add a 3 digit CVV code.');
@@ -245,12 +288,14 @@ const validateCvv = function () {
     }
 };
 
+// We listen for all the input fields and validate them on focus out
 nameField.addEventListener('blur', validateName);
 emailField.addEventListener('blur', validateEmail);
 ccNumber.addEventListener('blur', validateCCNumber);
 ccZip.addEventListener('blur', validateCCZip);
 ccCvv.addEventListener('blur', validateCvv);
 
+// The submit button validates all the fields and checkboxes on being clicked
 submitButton.addEventListener('click', function (event) {
     validateName();
     validateEmail();
@@ -260,10 +305,16 @@ submitButton.addEventListener('click', function (event) {
 });
 
 // We initialize all our functions
+// We set the focus on the name field
 window.onload = initialFocus();
+// We set the default payment, in this case credit card
 window.onload = defaultPayment();
+// We hide the job role "Other" field, since it will be controlled by the job role dropdown
 hideElement(otherJobTitle);
+// We hide the t-shirt colors, since they will be controlled by the design dropdown
 hideElement(tShirtColors);
+// Define the parallel tracks that cannot be selected at the same time
 parallelTracks(activityJSFrameworks, activityExpress);
 parallelTracks(activityJSLibs, activityNode);
+// We append the sum container that will be used to show the total price of the activities 
 activities.appendChild(createSumContainer);
